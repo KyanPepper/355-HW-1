a73aed365f698961964892e8d498086f6f53b674
kyanpepper <kyankks1@gmail.com>
2024-09-15 20:35:19 -0700

    Test cases

--- main.hs
+++ main.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use foldr" #-}
 
+import Test.HUnit
+
 dropList :: Int -> [a] -> [a]
 dropList x [] = []  --On empty list return empty list
 dropList 0 xs = xs  --On n = 0 return remaining list
@@ -61,11 +63,67 @@ insertHelper x (y:ys) -- Inserts new element x into correct position in sorted l
 
 
 
-insertionSort :: Ord a => [a] -> [a]
-insertionSort [] = []  -- if list is empty return empty list
-insertionSort (x:xs) = insertHelper x (insertionSort xs) --break down list element by element recursively till sorted see insertHelper for implementation 
 
+-- Test cases for dropList
+testDropList :: Test
+testDropList = TestList [
+    TestCase (assertEqual "dropList 3 [1,2,3,4,5]" [4,5] (dropList 3 [1,2,3,4,5])),
+    TestCase (assertEqual "dropList 0 [1,2,3,4,5]" [1,2,3,4,5] (dropList 0 [1,2,3,4,5])),
+    TestCase (assertEqual "dropList 2 []" [] (dropList 2 []))
+    ]
 
+-- Test cases for splitAtIndex
+testSplitAtIndex :: Test
+testSplitAtIndex = TestList [
+    TestCase (assertEqual "splitAtIndex 3 [1,2,3,4,5]" ([1,2,3], [4,5]) (splitAtIndex 3 [1,2,3,4,5])),
+    TestCase (assertEqual "splitAtIndex 0 [1,2,3,4,5]" ([], [1,2,3,4,5]) (splitAtIndex 0 [1,2,3,4,5])),
+    TestCase (assertEqual "splitAtIndex 2 []" ([], []) (splitAtIndex 2 []))
+    ]
 
+-- Test cases for concatLists
+testConcatLists :: Test
+testConcatLists = TestList [
+    TestCase (assertEqual "concatLists [1,2,3] [4,5]" [1,2,3,4,5] (concatLists [1,2,3] [4,5])),
+    TestCase (assertEqual "concatLists [] [4,5]" [4,5] (concatLists [] [4,5])),
+    TestCase (assertEqual "concatLists [1,2,3] []" [1,2,3] (concatLists [1,2,3] []))
+    ]
 
+-- Test cases for interleaveLists
+testInterleaveLists :: Test
+testInterleaveLists = TestList [
+    TestCase (assertEqual "interleaveLists [1,3,5] [2,4,6]" [1,2,3,4,5,6] (interleaveLists [1,3,5] [2,4,6])),
+    TestCase (assertEqual "interleaveLists [] [2,4,6]" [2,4,6] (interleaveLists [] [2,4,6])),
+    TestCase (assertEqual "interleaveLists [1,3,5] []" [1,3,5] (interleaveLists [1,3,5] []))
+    ]
 
+-- Test cases for mergeAscending
+testMergeAscending :: Test
+testMergeAscending = TestList [
+    TestCase (assertEqual "mergeAscending [1,3,5] [2,4,6]" [1,2,3,4,5,6] (mergeAscending [1,3,5] [2,4,6])),
+    TestCase (assertEqual "mergeAscending [] [2,4,6]" [2,4,6] (mergeAscending [] [2,4,6])),
+    TestCase (assertEqual "mergeAscending [1,3,5] []" [1,3,5] (mergeAscending [1,3,5] []))
+    ]
+
+-- Test cases for mergeDescending
+testMergeDescending :: Test
+testMergeDescending = TestList [
+    TestCase (assertEqual "mergeDescending [5,3,1] [6,4,2]" [6,5,4,3,2,1] (mergeDescending [5,3,1] [6,4,2])),
+    TestCase (assertEqual "mergeDescending [] [6,4,2]" [6,4,2] (mergeDescending [] [6,4,2])),
+    TestCase (assertEqual "mergeDescending [5,3,1] []" [5,3,1] (mergeDescending [5,3,1] []))
+    ]
+
+-- Test cases for mergeSort
+testMergeSort :: Test
+testMergeSort = TestList [
+    TestCase (assertEqual "mergeSort [3,1,4,1,5,9,2,6,5,3,5]" [1,1,2,3,3,4,5,5,5,6,9] (mergeSort [3,1,4,1,5,9,2,6,5,3,5])),
+    TestCase (assertEqual "mergeSort []" [] (mergeSort [])),
+    TestCase (assertEqual "mergeSort [1]" [1] (mergeSort [1]))
+    ]
+
+-- Test cases for insertionSort
+testInsertionSort :: Test
+testInsertionSort = TestList [
+    TestCase (assertEqual "insertionSort [3,1,4,1,5,9,2,6,5,3,5]" [1,1,2,3,3,4,5,5,5,6,9] (insertionSort [3,1,4,1,5,9,2,6,5,3,5])),
+    TestCase (assertEqual "insertionSort []" [] (insertionSort [])),
+    TestCase (assertEqual "insertionSort [1]" [1] (insertionSort [1]))
+    ]
